30aaa09950dda37b69fe72a4ab0a6214d8a9460e
Guy Lin <xup6ru03xu4@gmail.com>
2019-08-18 16:47:24 +0800

    all pass

---
+++ TwoSum/TwoSumKata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Messaging;
+
+namespace TwoSum
+{
+    public class TwoSumKata
+
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var result = new int[] { };
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var remains = target - nums[i];
+                var range = nums.ToList().GetRange(i + 1, nums.Length - (i + 1));
+                if (range.Contains(remains))
+                {
+                    result = new int[] { i, range.IndexOf(remains) + (i + 1) };
+                }
+            }
+
+            return result;
+        }
+    }
+}
